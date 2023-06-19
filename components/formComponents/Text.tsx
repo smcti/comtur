73ef761829c3daf34f77fcde4c701991e446d078
satilpereira <satil.prog@gmail.com>
2023-06-19 10:50:41 -0300

    fixed min width on input text field

--- components/formComponents/Text.tsx
+++ components/formComponents/Text.tsx
@@ -1,6 +1,6 @@
 const Text = (props: any) => {
   return (
-    <input className="border px-4 py-1 rounded-md" type="text" id={props.value} name={props.name} step={props.goTo} placeholder={props.placeholder}></input>
+    <input className="border w-full px-4 py-1 rounded-md" type="text" id={props.value} name={props.name} step={props.goTo} placeholder={props.placeholder}></input>
   )
 }
 
