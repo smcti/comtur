4dabf1ab36461bb0b22a4932b90ee0bfbac8a6c2
LuizKramer <luizeduardockramer@gmail.com>
2023-06-20 13:36:13 -0300

    update

--- utils/helpers/sheets.ts
+++ utils/helpers/sheets.ts
@@ -1,22 +1,22 @@
 const authSheets = require('utils/helpers/sheetsAuth.ts');
 
-exports.verifyEmail = async (email: string) =>{
+exports.verifyEmail = async (email: string) => {
     const { googleSheets, auth, spreadsheetId } = await authSheets();
 
-    const headerRange = "COMTUR!1:1"; 
+    const headerRange = "COMTUR!1:1";
     const response = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
         range: headerRange,
     });
-    
+
     if (!response.data.values) {
         throw new Error("No column headers found");
     }
-    
+
     const headers = response.data.values[0];
     const emailColumnIndex = headers.indexOf("email");
-    
+
     if (emailColumnIndex === -1) {
         throw new Error("Email column not found");
     }
@@ -26,11 +26,24 @@ exports.verifyEmail = async (email: string) =>{
         spreadsheetId,
         range: valuesRange,
     });
-    
+
     if (!emailResponse.data.values) {
         throw new Error("No email values found");
     }
-    
-    const emailValues = emailResponse.data.values.flat(); 
+
+    const emailValues = emailResponse.data.values.flat();
     return emailValues.includes(email);
 };
+
+exports.updateValues = (obj:any, keyMappings:any) => {
+    let newObj:any = {};
+    for (let key in obj) {
+      if (keyMappings.hasOwnProperty(key) && keyMappings[key].hasOwnProperty(obj[key])) {
+        newObj[key] = keyMappings[key][obj[key]];
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  };
+  
