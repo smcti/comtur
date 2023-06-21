10c24e1b86cd98d8c0250467e7df371e52fc2068
satilpereira <satil.prog@gmail.com>
2023-06-21 12:52:16 -0300

    Wip - footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,33 +5,42 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row brightness-0 invert justify-between w-full gap-16">
           <div className="flex flex-col w-fit gap-16">
             <h3 className="text-3xl w-full">Realização</h3>
-            <div className="flex flex-col w-fit">
-              <h3 className="font-bold clamp-contur border-b border-white">COMTUR</h3>
-              <p className="clamp-contur-s leading-4">Conselho Municipal de Turismo</p>          
+            <div className="max-h-12 w-fit">
+              <img className="max-h-12 w-auto" src="/assets/icons/comtur.png" alt="logo" />
             </div>
           </div>
           <div className="w-full flex flex-col gap-16">
-            <h3 className="text-3xl w-full">Parceiros</h3>
+            <h3 className="text-3xl w-full">Em parceria com</h3>
             <div className="flex flex-row w-full justify-between gap-8">
-              <div className="max-h-12 w-fit">
-                <img className="max-h-12 w-auto" src="/assets/images/cacispar.png" alt="Logo cacispar" />
-              </div>
               <div className="max-h-12 w-fit">
                 <img className="max-h-12 w-auto" src="/assets/images/logomarca_agencia_regional.png" alt="logo" />
               </div>
               <div className="max-h-12 w-fit">
                 <img className="max-h-12 w-auto" src="/assets/images/326444839_2333795833453568_810799049919276080_n.png" alt="logo" />
               </div>
+              <div className="max-h-12 w-fit">
+                <img className="max-h-12 w-auto" src="/assets/images/cacispar.png" alt="Logo cacispar" />
+              </div>
             </div>
           </div>
-          
+
           {/* <img className="max-h-12" src="#" alt="logo" /> */}
         </div>
       </div>
       <div className="section-default flex flex-col gap-4 text-center  pt-16">
-        <div className="text-2xl text-zircon-100"><span className="text-zircon-50">PB</span> Forms</div>
+        <div className="text-2xl text-gray-300"><span className="text-gray-50">PB</span> Forms</div>
         <hr />
-        <div>Desenvolvido pela <a className="underline" href="https://patobranco.tec.br/">Secretaria de Ciência, Tecnologia e Inovação de Pato Branco</a></div>
+        <div className="flex flex-col gap-4">
+          <div>
+            Desenvolvido e disponibilizado pela
+          </div>
+          <a className="underline" href="https://patobranco.tec.br/" target="_blank">
+            <div className="w-full flex flex-col items-center">
+              <img className="h-12" src="/assets/icons/logo-secretaria-white.svg" alt="Brasão Secretaria Municipal de Ciência, Tecnologia e Inovação" />
+            </div>
+          </a>
+        </div>
+
       </div>
     </footer>
   )
--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <nav className="h-20 w-full bg-indigo-500 text-zircon">
       <div className="section-default p-0 pl-4 h-full flex items-center">
-        <img className="h-12" src="/assets/icons/comtur.png" alt="" />
+        <img className="h-14" src="/assets/icons/comtur.png" alt="" />
         {/* <div className="font-black font-poppins">COMTUR</div> */}
       </div>
     </nav>
