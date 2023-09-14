6eabf7047ebec4bdc3ba154e82d862c9a7497728
satilpereira <satil.prog@gmail.com>
2023-09-14 19:24:45 -0300

    add loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import { BiLoaderAlt } from "react-icons/bi";
 
 const loading = () => {
   return (
-    <div className='max-w-2xl flex flex-row gap-4 my-32 items-center w-full p-4 mx-auto'>
+    <section className='max-w-2xl flex flex-row gap-4 my-32 items-center w-full p-4 mx-auto'>
       loading <BiLoaderAlt className='animate-spin' />
-    </div>
+    </section>
   );
 };
 
