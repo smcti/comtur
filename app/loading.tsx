b3d97c506e8c0d80bb10f9f2dbb45925ff6b6904
satilpereira <satil.prog@gmail.com>
2023-09-14 19:20:40 -0300

    add loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,11 @@
+import { BiLoaderAlt } from "react-icons/bi";
+
 const loading = () => {
   return (
-    <div>loading...</div>
-  )
-}
+    <div className='max-w-2xl flex flex-row gap-4 my-32 items-center w-full p-4 mx-auto'>
+      loading <BiLoaderAlt className='animate-spin' />
+    </div>
+  );
+};
 
-export default loading
+export default loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
 const page = () => {
-  return <div>Hello</div>;
+  redirect("/inventario");
+  return <div></div>;
 };
 
 export default page;
