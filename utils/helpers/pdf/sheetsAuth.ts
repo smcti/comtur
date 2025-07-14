a69ce1dd6450d111d704f961753b9cc5aa6d1b1f
Secretaria de Ciência, Tecnologia e Inovação de Pato Branco <admin@patobranco.tec.br>
2025-07-14 11:06:00 -0300

    Update sheetsAuth.ts

--- utils/helpers/pdf/sheetsAuth.ts
+++ utils/helpers/pdf/sheetsAuth.ts
@@ -1,4 +1,4 @@
-const { google } = require("googleapis");
+/* const { google } = require("googleapis");
 // import google from "googleapis";
 
 async function gGetAuthSheets() {
@@ -28,6 +28,27 @@ async function gGetAuthSheets() {
     client,
     googleSheets,
     spreadsheetId,
+  };
+}
+
+module.exports = gGetAuthSheets; */
+// Em: utils/helpers/pdf/sheetsAuth.ts
+
+const { google } = require("googleapis");
+
+async function gGetAuthSheets() {
+  // Esta é a forma correta de autenticar em ambientes GCP como o Cloud Run.
+  // Ele usará automaticamente a identidade da Conta de Serviço do seu serviço.
+  const auth = new google.auth.GoogleAuth({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
+  return {
+    auth,
+    googleSheets: sheets,
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
   };
 }
 
